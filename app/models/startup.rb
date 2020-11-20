0cdd35345a1fd143b66e22bc51ed8faa78b729a6
Molly <dolanmollye@gmail.com>
2020-11-20 11:16:36 -0800

    finished startup

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Startup
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+ 
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        self.domain = domain
+        self.name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        self.all.find{|first_startup| first_startup.founder == founder_name}
+    end
+
+    def self.domains
+        self.all.map{|startup| startup.domain}
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,8 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-
+s1 = Startup.new("Spotify", "Daniel Ek", "Domain1")
+s2 = Startup.new("Flatiron", "Ix", "Domain2")
+s3 = Startup.new("Instagram", "Adam", "Domain3")
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
